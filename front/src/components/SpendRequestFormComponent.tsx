8ce8ddad3814a3e07d7bcd9c69fb3aa24d8d3fb1
Gild-shogi <ok2336.coffee@gmail.com>
2024-08-27 10:41:40 +0900

    rollback and cherrypick

--- front/src/components/SpendRequestFormComponent.tsx
+++ front/src/components/SpendRequestFormComponent.tsx
@@ -7,7 +7,7 @@ import { useAuth } from '../hooks/useAuth';
 const SpendRequestForm = () => {
     const [count, setCount] = useState(0);
     const { currentUser, token, logout, setCurrentUser } = useAuth();
-    const {events, loading, error} = useCalendar();
+        const {events, loading, error} = useCalendar();
     const [isOverlay, setIsOverlay] = useState(false);
 
     const showOverlay = () => {
@@ -136,20 +136,23 @@ const SpendRequestForm = () => {
     
     return (
         <form action="" method="post" onSubmit={handleSubmit} className="w-full h-full">
-            <div className="w-full h-full flex flex-col">
-                {/* 入力フォームヘッダー */}
-                <div className="h-fit flex-none text-3xl p-2">
-                    アクションによって変更
-                </div>
-                {/* 入力フォームボディ */}
-                <div className="w-full h-full flex-grow flex overflow-auto">
+        <div className="w-full h-full flex flex-col">
+            {/* 入力フォームヘッダー */}
+            <div className="h-fit flex-none text-3xl p-2">
+                アクションによって変更
+            </div>
+            {/* 入力フォームボディ */}
+            <div className="w-full h-full flex-grow flex overflow-auto">
+                {/* 画像のアップロードおよびプレビュー機能を実装したい */}
+                    {/* ------------------------- */}
                     <div className="w-full h-full">
                         <div className="w-full flex">
-                            <div className="w-1/2 p-4">
+                            <div className="w-1/2  p-4">
                                 <input type="file" name="image_save" accept="img/jpg, img/png" onChange={handleInputChange}></input>
                             </div>
                             <div className="flex w-1/2 flex-col">
                                 <div className="mb-4 flex flex-row">
+                                    <div className="w-1/2">
                                     <div>
                                         {isOverlay && loading && <p>Loading...</p>}
                                         {isOverlay && error && <p>Error: {error.message}</p>}
@@ -166,8 +169,9 @@ const SpendRequestForm = () => {
                                             <button class="bg-blue-400 text-white" onClick={showOverlay}>Googleカレンダーから入力</button>
                                         </div>}
                                     </div>
+                                    </div>
                                     <div className="w-1/2 self-center">
-                                        <p>何個目の経費か</p>
+                                        <p>1</p>
                                     </div>
                                 </div>
                                 <div className="mx-auto w-full p-3 ">
@@ -197,19 +201,19 @@ const SpendRequestForm = () => {
                                     <div className="my-2">
                                         <label className="text-xl block text-gray-800">目的<span className="text-red-600 text-base">*</span></label>
                                         <div className="mt-1">
-                                            <input type="text" name="purpose" onChange={handleInputChange} className="inputcss" required/>
+                                            <input type="text" name="purpose" onChange={handleInputChange}  className="inputcss" required/>
                                         </div>
                                     </div>
                                     <div className="my-2">
-                                        <label className="text-xl block text-gray-800">適格請求書番号</label>
+                                        <label className="text-xl block text-gray-800">的確請求書番号</label>
                                         <div className="mt-1">
-                                            <input type="number" name="invoice_number" onChange={handleInputChange} className="inputcss" placeholder="0" />
+                                            <input type="number" name="invoice_number" onChange={handleInputChange} className="inputcss"/>
                                         </div>
                                     </div>
                                     <div className="my-2">
                                         <label className="text-xl block text-gray-800">連絡請求番号</label>
                                         <div className="mt-1">
-                                            <input type="number" name="contact_number" onChange={handleInputChange} className="inputcss" placeholder="0" />
+                                            <input type="number" name="contact_number" onChange={handleInputChange}  className="inputcss"/>
                                         </div>
                                     </div>
                                     <div className="my-2">
@@ -221,18 +225,89 @@ const SpendRequestForm = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="h-full flex-none">
-                        <div className="flex">
-                            <button type="submit" className="p-3 bg-blue-500 text-white rounded">登録する</button>
-                            <button type="button" onClick={addCounter} className="p-3 bg-gray-500 text-white rounded ml-2">追加する</button>
-                            <button type="button" onClick={subCounter} className="p-3 bg-gray-500 text-white rounded ml-2">削除する</button>
-                        </div>
-                    </div>
+
+                {(() => {
+                    const items = [];
+                    for (let i = 0; i < count; i++) {
+                        items.push(
+                            <div className="flex">
+                                <div className="w-1/2 min-w-fit p-4">
+                                    <input type="file" name="example" accept="img/jpg, img/png"></input>
+                                </div>
+                                <div className="w-1/2 p-4">
+                                    <div className="w-1/2 self-center">
+                                        <p>{count + 1}</p>
+                                    </div>
+                                    <div className="mx-auto w-full h-full p-3 ">
+                                        <div className="my-2">
+                                            <label className="text-xl block text-gray-800">利用日<span className="text-red-600 text-base">*</span></label>
+                                            <input datepicker id="default-datepicker" type="date" className="inputcss" placeholder="Select date"/>
+                                        </div>
+                                        <div className="my-2">
+                                            <label for="example11" className="text-xl block text-gray-800">金額<span className="text-red-600 text-base">*</span></label>
+                                            <div className="relative">
+                                                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center px-2.5 text-gray-500">\</div>
+                                                <div className="absolute inset-y-0 right-0 flex items-center text-gray-500">
+                                                    <label for="currency" className="sr-only">Currency</label>
+                                                </div>
+                                                <input type="number" id="example11" className="inputcss pl-8" placeholder="0" />
+                                            </div>
+                                        </div>
+                                        <div className="my-2">
+                                            <label className="text-xl block text-gray-800">経費科目<span className="text-red-600 text-base">*</span></label>
+                                            <div className="mt-1">
+                                                <input type="text" name="example-required-input" className="inputcss"/>
+                                            </div>
+                                        </div>
+                                        <div className="my-2">
+                                            <label className="text-xl block text-gray-800">的確請求書番号</label>
+                                            <div className="mt-1">
+                                                <input type="text" name="invoice_number"  className="inputcss"/>
+                                            </div>
+                                        </div>
+                                        <div className="my-2">
+                                            <label className="text-xl block text-gray-800">連絡請求番号</label>
+                                            <div className="mt-1">
+                                                <input type="text" name="example-text-input"  className="inputcss"/>
+                                            </div>
+                                        </div>
+                                        <div className="my-2">
+                                            <label className="text-xl block text-gray-800">メモ</label>
+                                            <div className="mt-1">
+                                                <textarea id="story" name="story" rows="5" className="inputcss"></textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                    return (
+                            <div>{items}
+                                <div className="h-12 flex-none bottom-0 flex items-center justify-end bg-gray-100 px-5">
+                                    <div className="flex gap-4 px-2">
+                                        <button type="button" onClick={subCounter} className="bg-red-500 text-white px-4 py-2">削除</button>
+                                        <button type="button" onClick={addCounter} className="bg-green-500 text-white px-4 py-2">追加</button>
+                                    </div>
+                                </div>
+                            </div>
+                    );
+                })()}
+                </div>
+                     
+                
+            </div>
+            
+            {/* 入力フォームフッター */}
+            <div className="h-12 flex-none flex items-center justify-end bg-gray-100 px-5">
+                <div className="flex gap-4 px-2">
+                    <button className="bg-red-500 text-white px-4 py-2">キャンセル</button>
+                    <button type="submit"  className="bg-green-500 text-white px-4 py-2">申請</button>
                 </div>
             </div>
+        </div>
         </form>
-    );
-};
+    )
+}
 
 export default SpendRequestForm;
