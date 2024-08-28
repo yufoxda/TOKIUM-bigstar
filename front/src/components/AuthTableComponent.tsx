3d8ff197c52edb116d5e0df1941845c29b01cd7a
Gild-shogi <ok2336.coffee@gmail.com>
2024-08-28 23:48:26 +0900

    add pagenation

--- front/src/components/AuthTableComponent.tsx
+++ front/src/components/AuthTableComponent.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import { useCsrf } from '../hooks/useCsrf';
 import { useGetAllSpendItem } from '../hooks/useGetAllSpendItem';
@@ -22,12 +23,17 @@ interface SpendRequest {
     spend_request_item: SpendRequestItem[];
 }
 
-
-
 const AuthTableComponent = () => {
-    const { currentUser, token } = useAuth();
+    const { token } = useAuth();
     const { csrfToken } = useCsrf();
     const { spend } = useGetAllSpendItem<SpendRequest>();
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
+
+    // ページネーション用のデータ
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = spend.slice(indexOfFirstItem, indexOfLastItem);
 
     const changeStatus = (spend_id: string, status: string) => {
         fetch(`${API_URL}/keihi/change_status?id=${spend_id}&status=${status}`, {
@@ -37,42 +43,25 @@ const AuthTableComponent = () => {
               "X-CSRF-Token": csrfToken,
               "Content-Type": "application/json",
             },
-          }
-
-        ).catch((error) => {
-            console.error("Error fetching current user", error);
+          }).catch((error) => {
+            console.error("Error changing status", error);
           });
           window.location.reload();
     }
 
+    // ページの移動
+    const nextPage = () => setCurrentPage(currentPage + 1);
+    const prevPage = () => setCurrentPage(currentPage - 1);
+    const totalPages = Math.ceil(spend.length / itemsPerPage);
 
     return (
         <div className="container mx-auto p-4">
-            {spend.map((item, index) => (
+            {currentItems.map((item, index) => (
                 <div key={index} className="mb-8">
-                    {console.log(item)}
                     <p className="text-lg font-bold">申請者: <span className="font-normal">{item.user_name}</span></p>
                     <p className="text-lg font-bold">申請状況: <span className="font-normal">{item.status}</span></p>
                     <p className="text-lg font-bold">申請先: <span className="font-normal">{item.spend_to}</span></p>
                     <p className="text-lg font-bold">目的: <span className="font-normal">{item.purpose}</span></p>
-                    <div className="flex space-x-4 mt-2">
-                        <button
-                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                            onClick={() => changeStatus(item.id, 'approve')}>
-                            承認
-                        </button>
-                        <button
-                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                            onClick={() => changeStatus(item.id, 'reject')}>
-                            拒否
-                        </button>
-                        <button
-                            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
-                            onClick={() => changeStatus(item.id, 'pending')}>
-                            審査中に戻す
-                        </button>
-                    </div>
-
 
                     <p className="text-lg font-bold mt-4">申請項目: </p>
                     <table className="min-w-full bg-white border border-gray-200 mt-2">
@@ -99,8 +88,44 @@ const AuthTableComponent = () => {
                             ))}
                         </tbody>
                     </table>
+
+                    <div className="flex space-x-4 mt-4">
+                        <button
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={() => changeStatus(item.id, 'approve')}>
+                            承認
+                        </button>
+                        <button
+                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={() => changeStatus(item.id, 'reject')}>
+                            拒否
+                        </button>
+                        <button
+                            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={() => changeStatus(item.id, 'pending')}>
+                            審査中に戻す
+                        </button>
+                    </div>
                 </div>
             ))}
+
+            {/* ページネーション */}
+            <div className="flex justify-center mt-4">
+                <button
+                    onClick={prevPage}
+                    disabled={currentPage === 1}
+                    className={`px-4 py-2 mr-2 bg-gray-300 text-gray-700 rounded ${currentPage === 1 ? 'opacity-50 cursor-not-allowed' : 'hover:bg-gray-400'}`}
+                >
+                    前へ
+                </button>
+                <button
+                    onClick={nextPage}
+                    disabled={currentPage === totalPages}
+                    className={`px-4 py-2 bg-gray-300 text-gray-700 rounded ${currentPage === totalPages ? 'opacity-50 cursor-not-allowed' : 'hover:bg-gray-400'}`}
+                >
+                    次へ
+                </button>
+            </div>
         </div>
     );
 }
